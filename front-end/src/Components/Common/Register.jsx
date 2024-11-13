cef6cfb2deec6953883e29623d51b49e3e689ceb
manicdon7 <manicdon7@gmail.com>
2024-11-13 14:10:36 +0530

    admin page corrected

--- front-end/src/Components/Common/Register.jsx
+++ front-end/src/Components/Common/Register.jsx
@@ -141,6 +141,7 @@ const Register = () => {
                         <Dropdown.Menu>
                            <Dropdown.Item onClick={() => handleSelect("Student")}>Student</Dropdown.Item>
                            <Dropdown.Item onClick={() => handleSelect("Teacher")}>Teacher</Dropdown.Item>
+                           <Dropdown.Item onClick={() => handleSelect("Admin")}>Admin</Dropdown.Item>
                         </Dropdown.Menu>
                      </Dropdown>
                      <Box mt={2}>
